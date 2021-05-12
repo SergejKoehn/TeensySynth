7bce4f613bfc599f3985ebb90a550c7e7e7bc442
Sergej Koehn <koehn@tetronik.com>
2021-05-12 13:56:50 +0200

    tft and touch added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+#include "Adafruit_ILI9341_8bit.h"
+#include "TouchScreen.h"
+
 #include "synth_voice.h"
+
+Adafruit_ILI9341_8bit_STM tftDisplay;
+TouchScreen               tftTouch(1,2,3,4,200);
 
 void setup() {
   // put your setup code here, to run once:
+  tftDisplay.begin();
 }
 
 void loop() {
